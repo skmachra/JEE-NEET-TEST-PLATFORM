348db0cfe0742fe4f5247d3bcd7ce52697ad1aaa
Sunil <sunilmachra2021@gmail.com>
2025-02-06 21:55:17 +0530

    update Vite configuration to use production backend URL for API proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy : {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://jee-neet-test-platform-backend.vercel.app',
         changeOrigin: true,
       }
     }
